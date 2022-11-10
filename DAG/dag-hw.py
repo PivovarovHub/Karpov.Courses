7902b82d5916ca6797dd11a20eeddda9d2927fb3
Aleksei Pivovarov <pivovarov.live@yandex.com>
2022-11-10 16:30:34 +0300

    Update dag-hw.py
    Еще одно небольшое изменение комментирования

--- DAG/dag-hw.py
+++ DAG/dag-hw.py
@@ -25,13 +25,13 @@ y = 1994 + hash(f'a-pivovarov-25') % 23
 
 def a_pivovarov_hw_3():
     
-    ''' Создаем функцию, которая считывает данные из файла '''
+    '''Создаем функцию, которая считывает данные из файла'''
     @task()
     def get_data():
         data = pd.read_csv(vgsales)
         return data
     
-    ''' Создаем функцию, которая находит, какая игра была самой продаваемой в этом году во всем мире '''
+    '''Создаем функцию, которая находит, какая игра была самой продаваемой в этом году во всем мире'''
     @task()
     def get_top_name(data):
         global_top = data.groupby('Name', as_index=False) \
@@ -40,7 +40,7 @@ def a_pivovarov_hw_3():
                         .iloc[0,0]
         return global_top
     
-    ''' Создаем функцию, которая находит, игры какого жанра были самыми продаваемыми в Европе и возвращает список '''
+    '''Создаем функцию, которая находит, игры какого жанра были самыми продаваемыми в Европе и возвращает список'''
     @task()
     def get_eu_genre(data):
         top_eu_genre = data.groupby('Genre', as_index=False).EU_Sales.sum().sort_values('EU_Sales', ascending=False)
@@ -61,20 +61,20 @@ def a_pivovarov_hw_3():
         top_platform_NA = top_platform_NA[top_platform_NA.Number == top_platform_NA.Number.max()].Platform.to_list()
         return top_platform_NA
     
-    ''' Создаем функцию, которая находит, у какого издателя самые высокие средние продажи в Японии и возыращает список '''
+    '''Создаем функцию, которая находит, у какого издателя самые высокие средние продажи в Японии и возыращает список'''
     @task()
     def get_jp_publisher(data):
         top_publisher_JP = data.groupby('Publisher', as_index=False).JP_Sales.mean().sort_values('JP_Sales', ascending=False)
         top_publisher_JP = top_publisher_JP[top_publisher_JP.JP_Sales == top_publisher_JP.JP_Sales.max()].Publisher.to_list()
         return top_publisher_JP
     
-    ''' Создаем функцию, которая находит, у какого издателя самые высокие средние продажи в Японии '''
+    '''Создаем функцию, которая находит, у какого издателя самые высокие средние продажи в Японии'''
     @task()
     def get_games_eu_vs_jp(data):
         EU_beat_JP =  data.query('EU_Sales > JP_Sales').shape[0]
         return EU_beat_JP
 
-    ''' Создаем функцию, которая выводит все на печать '''
+    '''Создаем функцию, которая выводит все на печать'''
     @task()
     def print_data(top_game, eu_genre, na_platform, jp_publisher, games_eu_vs_jp):
         print(f'''
